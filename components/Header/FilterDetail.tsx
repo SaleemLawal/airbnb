a8800210769005594d6a579de21e114c8ccf485f
saleemlawal <slawal1@umbc.edu>
2025-02-24 00:22:36 -0500

    Add calendar and completed filter component

---
+++ components/Header/FilterDetail.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { PiMinusCircleThin, PiPlusCircleThin } from "react-icons/pi";
+import { Button } from "../ui/button";
+
+interface FilterDetailProps {
+  title: string;
+  description: string;
+  borderApply?: boolean;
+}
+export default function FilterDetail({ title, description, borderApply }: FilterDetailProps) {
+  return (
+    <div className={`flex w-full flex-wrap items-center justify-between gap-4 ${borderApply ? "border-b-[1px]" : ""}`}>
+      <div className="flex flex-col">
+        <div>{title}</div>
+        <p className="text-muted-foreground text-sm">{description}</p>
+      </div>
+
+      <div className="text-muted-foreground flex items-center justify-center space-x-2">
+        <PiMinusCircleThin className="h-8 w-8 cursor-pointer" />
+        <div className="w-6 text-center">1</div>
+        <PiPlusCircleThin className="h-8 w-8 cursor-pointer" />
+      </div>
+    </div>
+  );
+}
